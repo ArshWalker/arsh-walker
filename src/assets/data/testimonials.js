412327fc580bd1036adc3ddb7c84a59e00f9ea41
ArshWalker <arshwalker7@gmail.com>
2021-07-20 00:27:58 +0530

    testimonials dynamic data added

---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,26 @@
+const testimonials = [
+  {
+    id: 1,
+    name: 'Arshdeep',
+    title: 'Web Developer',
+    org: 'Owner',
+    desc:
+      'Like this portfolio website? Your positive words helps me to fill the testimonials. You can provide them in contact page. Scroll Down!😉',
+  },
+  {
+    id: 2,
+    name: 'Dhammapada',
+    title: 'Book',
+    org: 'Buddhism',
+    desc:
+      'Do not give your attention to what others do or fail to do; give it to what you do or fail to do.. Also, his work is really amazing. Do really high-quality work.',
+  },
+  {
+    id: 3,
+    name: 'Plato',
+    title: 'Author',
+    org: 'philosopher',
+    desc: 'A good decision is based on knowledge and not on numbers.',
+  },
+];
+export default testimonials;
