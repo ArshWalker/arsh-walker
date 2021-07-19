4f7f804550465f782f7f9c66beb72c5723f8c0c8
ArshWalker <arshwalker7@gmail.com>
2021-07-20 00:39:07 +0530

    Herosection added

---
+++ src/components/HeroSection.js
@@ -0,0 +1,246 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
+import styled from 'styled-components';
+import HeroImg from '../assets/images/fog.jpg';
+import Button from './Button';
+import SocialMediaArrow from '../assets/images/social-media-arrow.svg';
+import ScrollDownArrow from '../assets/images/scroll-down-arrow.svg';
+import PText from './PText';
+
+const HeroStyles = styled.div`
+  background: url('https://mir-s3-cdn-cf.behance.net/project_modules/1400/eb7ba755625865.598c27c460739.png')
+    no-repeat center center;
+
+  background-size: cover;
+  .hero {
+    height: 100vh;
+    min-height: 1000px;
+    width: 100%;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+  }
+  .hero__heading {
+    font-size: 2rem;
+    margin-bottom: -4rem;
+    position: relative;
+    span {
+      display: inline-block;
+      width: 100%;
+    }
+    .hero__name {
+      /* font-family: 'Viksi Script', sans-serif !important; */
+      font-family: Viksi Script, cursive;
+      font-size: 7rem;
+      font-weight: 700;
+      color: var(--white);
+      /* letter-spacing: 0.7rem; */
+    }
+    .hero__hello {
+      color: #ffd523;
+      font-size: 1.8rem;
+    }
+  }
+  .hero__img {
+    max-width: 900px;
+    width: 100%;
+    height: 600px;
+    margin: 0 auto;
+    border: 2px solid var(--gray-1);
+  }
+  .hero__info {
+    margin-top: -18rem;
+    .para p {
+      color: #caf7e3;
+    }
+    sup {
+      color: #caf7e3;
+    }
+  }
+  .hero__social,
+  .hero__scrollDown {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    position: absolute;
+
+    bottom: 20px;
+    width: 50px;
+  }
+  .hero__social {
+    left: 50px;
+  }
+  .hero__scrollDown {
+    right: 50px;
+  }
+
+  .hero__social__indicator,
+  .hero__scrollDown {
+    width: 50px;
+    p {
+      font-size: 1.6rem;
+      transform: translateY(-70px) rotate(90deg);
+      letter-spacing: 0.7rem;
+      text-transform: uppercase;
+    }
+    img {
+      max-height: 45px;
+      width: 16px;
+      margin: 0 auto;
+      object-fit: contain;
+    }
+  }
+  .hero__scrollDown {
+    img {
+      max-height: 70px;
+    }
+  }
+
+  .hero__social__text {
+    ul {
+      li {
+        margin-bottom: 1rem;
+        a {
+          display: inline-block;
+          font-size: 2.6rem;
+          /* transform: rotate(-90deg); */
+          letter-spacing: 5px;
+          margin-bottom: 2rem;
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    .hero {
+      min-height: 750px;
+    }
+    .hero__heading {
+      font-size: 1.4rem;
+      margin-bottom: -3rem;
+      .hero__name {
+        font-size: 4.5rem;
+      }
+    }
+    .hero__img {
+      height: 300px;
+    }
+    .hero__info {
+      margin-top: 3rem;
+    }
+    .hero__social {
+      left: 0px;
+      bottom: -15%;
+      width: 20px;
+      .hero__social__indicator {
+        width: 20px;
+        p {
+          font-size: 1.2rem;
+        }
+        img {
+          max-height: 22px;
+        }
+      }
+      .hero__social__text {
+        ul {
+          li {
+            a {
+              font-size: 2.2rem;
+              margin-bottom: 0.2rem;
+            }
+          }
+        }
+      }
+    }
+    .hero__scrollDown {
+      right: 0;
+      width: 20px;
+      gap: 1rem;
+      p {
+        font-size: 1.3rem;
+      }
+    }
+  }
+`;
+
+export default function HeroSection() {
+  return (
+    <HeroStyles>
+      <div className="hero">
+        <div className="container">
+          <h1 className="hero__heading">
+            <span className="hero__hello">Hello, This is</span>
+            <span className="hero__name">ArsH WalkeR</span>
+          </h1>
+          <div className="hero__img">
+            <img src={HeroImg} alt="" />
+          </div>
+          <div className="hero__info">
+            <PText className="text">
+              I am 3<sup>rd</sup> year CS student. A passionate Web Developer
+              and Coding Enthusiast, learning through my experiences. I love to
+              create new web applications, funny games and Real life projects
+              for the cummunity.
+            </PText>
+            <Button btnText="Checkout my Works" btnLink="/projects" />
+          </div>
+          <div className="hero__social">
+            <div className="hero__social__indicator">
+              <p>Follow</p>
+              <img src={SocialMediaArrow} alt="icon" />
+            </div>
+            <div className="hero__social__text">
+              <ul>
+                <li>
+                  <a
+                    href="https://www.facebook.com/arshdeepsingh.saini.355"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaFacebook />
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="https://www.instagram.com/arsh_walker7/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaInstagram />
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="https://github.com/ArshWalker"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaGithub />
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/arshdeep-singh-saini-1b35571b7/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaLinkedin />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="hero__scrollDown">
+            <p>Scroll</p>
+            <img src={ScrollDownArrow} alt="ScrollDown Arrow" />
+          </div>
+        </div>
+      </div>
+    </HeroStyles>
+  );
+}
