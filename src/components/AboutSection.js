9999ad862a85cd3f4fcc204aeb32fcae3a9f60a6
ArshWalker <arshwalker7@gmail.com>
2021-07-20 00:36:34 +0530

    About Section added

---
+++ src/components/AboutSection.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from './Button';
+import PText from './PText';
+import SectionTitle from './SectionTitle';
+import AboutImg from '../assets/images/pngwing.com.png';
+
+const AboutSectionStyles = styled.div`
+  background-color: #ac0d0d;
+  padding: 10rem 0;
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    text-align: left;
+  }
+  .aboutSection__left,
+  .aboutSection__right {
+    flex: 1;
+    .aboutImg {
+      margin-top: -70px;
+    }
+  }
+  .section-title {
+    text-align: center;
+  }
+  .para {
+    margin-top: 2rem;
+    /* margin-left: 0; */
+    margin-left: 20%;
+  }
+  .aboutSection__buttons {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 2rem;
+    margin-top: 2rem;
+    margin-left: 20%;
+  }
+  @media only screen and (max-width: 1150px) {
+    .aboutSection__left {
+      flex: 4;
+    }
+    .aboutSection__right {
+      flex: 3;
+    }
+    .section-title {
+      margin-left: 8%;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      text-align: center;
+    }
+    .aboutSection__left,
+    .aboutSection__right {
+      width: 100%;
+    }
+    .aboutSection__right {
+      margin-top: 3rem;
+    }
+    .section-title {
+      text-align: center;
+    }
+    .para {
+      margin: 0 auto;
+      margin-top: 2rem;
+    }
+    .aboutSection__buttons {
+      flex-direction: column;
+      gap: 0rem;
+      margin-left: 0;
+      .button-wrapper,
+      a {
+        width: 100%;
+        text-align: center;
+      }
+    }
+  }
+`;
+
+export default function AboutSection() {
+  return (
+    <AboutSectionStyles>
+      <div className="container">
+        <div className="aboutSection__left">
+          <SectionTitle
+            subheading="Let me introduce myself"
+            heading="About Me"
+          />
+          <PText>
+            I am 3<sup>rd</sup> year CS student. A passionate Web Developer and
+            Coding Enthusiast, learning through experiences. I love to create
+            new web applications, funny games and Real life projects for the
+            cummunity.
+          </PText>
+          <div className="aboutSection__buttons">
+            <Button btnText="Works" btnLink="/projects" />
+            <Button btnText="Read More" btnLink="/about" outline />
+          </div>
+        </div>
+        <div className="aboutSection__right">
+          <img className="aboutImg" src={AboutImg} alt="Img" />
+        </div>
+      </div>
+    </AboutSectionStyles>
+  );
+}
